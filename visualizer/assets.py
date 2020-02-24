328d598f71b348bd976e623bb729396a2fbf884c
Simon Chervenak <sarras305@gmail.com>
2020-02-24 10:59:41 -0500

    add texture loading

--- visualizer/assets.py
+++ visualizer/assets.py
@@ -1,6 +1,6 @@
 from enum   import Enum
 from random import choice
-
+from ursina import load_texture
 class GroundType(Enum):
     BOTTOM = 0
     TOP = 1
@@ -9,6 +9,12 @@ class GroundType(Enum):
 
 class Theme(Enum):
     BLUE, BROWN, CASTLE, CHOCO, DIRT, GRASS, GREEN, METAL, PURPLE, SAND, SNOW, TUNDRA, YELLOW = range(13)
+
+def load_ground_textures():
+    for theme in Theme:
+        for typ in GroundType:
+            file = f"assets/tiles/{theme.name.lower()}/{typ.name.lower()}.png"
+            load_texture(f"{theme.name.lower()}_{typ.name.lower()}", file)
 
 def texture(c: str, typ: GroundType, theme: Theme=None) -> str:
     '''
@@ -19,9 +25,5 @@ def texture(c: str, typ: GroundType, theme: Theme=None) -> str:
     if theme is None:
         theme = choice(list(Theme))
 
-    file = None
     if c == 'G':
-        file = f"assets/tiles/{theme.name.lower()}/{typ.name.lower()}.png"
-
-        
-
+        return f"{theme.name.lower()}_{typ.name.lower()}"
