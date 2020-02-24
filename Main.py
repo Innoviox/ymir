a1fa6cba9c243b7a5ccc873876460de99cb4a987
sam <s.f.skarpiphi@gmail.com>
2020-02-24 09:46:37 -0500

    Basic setup

---
+++ Main.py
@@ -0,0 +1,6 @@
+from ursina import *
+from Control import Controller
+from Player import Player
+
+controller = Controller(Player(0,0,None))
+
---
+++ Player.py
@@ -0,0 +1,5 @@
+class Player():
+    def __init__(self, x, y, model):
+        self.x = x
+        self.y = y
+        self.model = model
