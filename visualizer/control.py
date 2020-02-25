4f244c7c161abf88032ee0420d97cf9d374336b8
sam <s.f.skarpiphi@gmail.com>
2020-02-24 20:23:50 -0500

    made player render in front of background

--- visualizer/control.py
+++ visualizer/control.py
@@ -1,5 +1,7 @@
 from ursina import *
 from ursina.input_handler import held_keys
+
+from visualizer.player import Player
 from visualizer.util import *
 import numpy as np
 from visualizer.assets import *
@@ -10,8 +12,7 @@ class Controller():
     dt = .1
     scale = 1
 
-    def __init__(self, player):
-        self.player = player
+    def __init__(self):
         self.app = Ursina()
         self.entities = []
 
@@ -39,11 +40,10 @@ class Controller():
 
     def start(self):
         load_ground_textures()
-
         reader = FileReader("visualizer/test_file.txt")
-
         self.build_from_array(reader.read())
-
+        self.player = Player(np.array([0,0],dtype='float64'),
+	Entity(model="cube",color=color.blue,scale=1))
         input_handler.bind('right arrow', 'd')
         input_handler.bind('left arrow', 'a')
         input_handler.bind('up arrow', 'w')
--- visualizer/main.py
+++ visualizer/main.py
@@ -1,10 +1,6 @@
-from ursina import *
 from visualizer.control import Controller
-from visualizer.player import Player
-import numpy as np
 
-controller = Controller(Player(np.array([0,0],dtype='float64'),
-	Entity(model="cube",color=color.blue,scale=1)))
+controller = Controller()
 
 def update():
     controller.update()
