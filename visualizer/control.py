8dc1fcad4d106bcff4c89978d48d2438c40e7f20
sam <s.f.skarpiphi@gmail.com>
2020-02-26 10:20:41 -0500

    Added method for determining what tiles a player has collided with

--- visualizer/control.py
+++ visualizer/control.py
@@ -11,41 +11,46 @@ import numpy as np
 from visualizer.assets import *
 from visualizer.FileReader import *
 
+scale = 1
+dt = .1
 
 class Controller():
-    dt = .1
-    scale = 1
-
     def __init__(self):
         self.app = Ursina()
         self.entities = []
-
+        self.tile_array = []
         camera.orthographic = True
         camera.fov = 25
 
         window.fullscreen = True
 
     def process_input(self):
-        self.player.input = self.dt * np.array([held_keys['d'] - held_keys['a'], held_keys['w'] - held_keys['s']])
+        self.player.input = dt * np.array([held_keys['d'] - held_keys['a'], held_keys['w'] - held_keys['s']])
 
     def update(self):
         self.process_input()
-        self.player.update_position_velocity(self.dt)
+        self.player.update_position_velocity(dt)
         self.player.update_render()
+        print(self.player_colliding())
+
+    #returns the ground tiles collided with, or an empty list for no collisions
+    def player_colliding(self):
+        collided_tiles = list(filter(lambda x: inside(self.player, x), get_nearby_ground_tiles(self.player.position, self.tile_array)))
+        return collided_tiles
 
     def build_from_array(self, array):
+        self.tile_array = array
         for y, row in enumerate(array):
             for x, tile in enumerate(row):
                 self.entities.append(Entity(model="cube",
                                             texture=tile.texture,
-                                            scale=self.scale,
-                                            position=(self.scale * tile.x, self.scale * tile.y, 0)))
+                                            scale=scale,
+                                            position=(scale * tile.x, scale * tile.y, 0)))
                 if tile.type == TileType.START:
                     self.starting_tile = tile
                 if tile.type == TileType.END:
                     self.ending_tile = tile
 
-    
     def start(self):
         load_ground_textures()
         reader = FileReader("visualizer/test_file.txt")
--- visualizer/util.py
+++ visualizer/util.py
@@ -1,4 +1,39 @@
 import math
+from visualizer import control
+from visualizer.tile import TileType
+
 
 def mag(a):
-    return math.sqrt(sum([x*x for x in a]))
+    return math.sqrt(sum([x * x for x in a]))
+
+
+# Tells if an entity (anything with an ordered pair position vector) is inside the given tile
+def inside(player, tile):
+    density = 10
+    for i in range(0, density):
+        if point_inside([player.position[0] + control.scale * i / density, player.position[1]], tile):
+            return True
+        if point_inside([player.position[0] + control.scale * i / density, player.position[1] + control.scale],
+                        tile):
+            return True
+        if point_inside([player.position[1], player.position[1] + control.scale * i / density], tile):
+            return True
+        if point_inside([player.position[1] + control.scale, player.position[1] + control.scale * i / density],
+                        tile):
+            return True
+    return False
+
+
+# tells if a position is inside a tile
+def point_inside(point, tile):
+    if tile.x <= point[0] <= tile.x + control.scale and tile.y <= point[1] <= tile.y + control.scale:
+        return True
+    return False
+
+
+# gets tiles near a location (in a 2x2)
+def get_nearby_ground_tiles(position, tile_array):
+    position /= control.scale
+    return list(filter(lambda x: x.type == TileType.GROUND, [tile_array[int(position[1])][int(position[0])],
+                                                             tile_array[int(position[1]) + 1][int(position[0])]
+        , tile_array[int(position[1])][int(position[0]) + 1], tile_array[int(position[1]) + 1][int(position[0]) + 1]]))
