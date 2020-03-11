406898811e99514d6c2fa3a0b88a002c68607d05
Simon Chervenak <sarras305@gmail.com>
2020-03-11 14:20:00 -0400

    implement teleporters, weird fallthrough bug

--- visualizer/sprite/tiles/TeleporterTile.py
+++ visualizer/sprite/tiles/TeleporterTile.py
@@ -1,12 +1,24 @@
 from visualizer.sprite.tile import Tile
 from ursina.input_handler import held_keys
+from ursina import Sequence, Func
 from visualizer.sprite.util import Hitbox
+from collections import defaultdict
+
+teleporter_locations = defaultdict(list)
+teleporter_delay = 1 # teleporter delay in seconds
+
 class TeleporterTile(Tile):
+    teleported = False
+
     def setup(self):
-        self.hitbox = Hitbox([0.4, 0.4, 0.6, 0.6])
+        teleporter_locations[self.type.char].append(self.position)
 
     def collide(self, tile, direction, commit=True):
-        # if commit and 'Player' in str(type(tile)):
-        #     print(held_keys)
-        #     print("teleporting")
+        if commit and 'Player' in str(type(tile)) and held_keys['s'] and not TeleporterTile.teleported:
+            TeleporterTile.teleported = True
+            Sequence(teleporter_delay, Func(self.set_teleported, False)).start()
+            self.controller.player.position = [i for i in teleporter_locations[self.type.char] if i != self.position][0]
         return False
+
+    def set_teleported(self, val):
+        TeleporterTile.teleported = val
