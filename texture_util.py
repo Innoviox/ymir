34d7fd3643fd6229ee51a94e343f8b94dd8c0e42
Aurathic <39442291+Aurathic@users.noreply.github.com>
2020-03-22 09:22:12 -0400

    Document texture_util.py

--- texture_util.py
+++ texture_util.py
@@ -1,7 +1,10 @@
 from PIL import Image, ImageOps
 from os import listdir
 
-def animate(from_file, to_file, slice_width):
+def slice_image(from_file, to_file, slice_width):
+    """Split up the image in the visualizer/textures/ directory with the file name
+    <from_file>.png into 'slices' of width <slice_width>,
+    and put them in files with names in the form <to_file>_[number].png. """
     from_img = Image.open("visualizer/textures/"+from_file+".png")
     to_img = Image.open("visualizer/textures/"+to_file+".png")
 
@@ -18,7 +21,11 @@ def animate(from_file, to_file, slice_width):
 
         width += slice_width
 
-def fix(file):
+def rescale_image(file):
+    """Crop and scale the image in the file with the name <file>.png
+    so that it fits within a 70 by 70 pixel image.
+    Specifically, scale it so that it has a width of 70 pixels, then
+    align it at the top of a new 70-by-70 image file with the same name."""
     img = Image.open(file + ".png")
     out = Image.new('RGBA', (70, 70), (0, 0, 0, 0))
 
@@ -29,6 +36,8 @@ def fix(file):
     out.save(file+".png")
 
 def _flip(file):
+    """Mirror the image at the location <file>. If the name as in the form <a>_<b>,
+    save it as <a>_flipped_<b>; otherwise, append _flipped to the name. """
     img = Image.open(file)
     img = ImageOps.mirror(img)
     try:
@@ -43,21 +52,20 @@ def _flip(file):
         print("couldn't flip", file, e)
     
 
-def flip(player_directory):
-    for file in listdir(player_directory):
+def flip(directory):
+    """Apply _flip to all PNG files in the given directory."""
+    for file in listdir(directory):
         if file.endswith(".png"):
-            _flip(player_directory + file)
+            _flip(directory + file)
 
 def transparify(file):
+    """Turns all fully opaque white pixels in an image <file>.png transparent."""
     img = Image.open(file+".png").convert("RGBA")
-
     out = Image.new("RGBA", img.size, (0, 0, 0, 0))
-
     for y in range(img.height):
         for x in range(img.width):
             color = img.getpixel((x, y))
             if color != (255, 255, 255, 255):
                 out.putpixel((x, y), color)
-
     out.save(file+".png", "PNG")
     out.show()
